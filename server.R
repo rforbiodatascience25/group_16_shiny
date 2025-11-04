81c1cda074728998bc49188078c946044ec57803
natalijensen1 <s253573@dtu.dk>
2025-11-04 12:04:37 +0100

    Update server.R

--- server.R
+++ server.R
@@ -1,2 +1,16 @@
 # Define the Server (Backend)
-server <- ...
+server <- function(input, output) {
+  output$dna <- renderText({
+    gene_dna(length = input$n_bases, base_probs = c(input$prob_A, input$prob_G, input$prob_C, input$prob_T))
+  })
+  
+  output$rna <- renderText({
+    transcribe_dna(dna=input$DNA_Seq)
+  })
+  
+  output$aa <- renderText ({
+    translate_rna(rna=input$RNA_Seq)
+  })
+}
+
+
